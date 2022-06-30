2dac0bdddfc453ce3621ccd12567355d48c75aa4
Eric Hayes <eric@deployfx.com>
2022-06-30 12:08:13 -0700

    Added support for OAuth2 proxy (off by default)

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN npm install --omit=dev
 COPY dist ./
 
 FROM golang:latest as go
-RUN go install -v github.com/bitly/oauth2_proxy@latest
+RUN go install -v github.com/oauth2-proxy/oauth2-proxy/v7@latest
 
 FROM node:18-alpine
 # https://stackoverflow.com/questions/66963068/docker-alpine-executable-binary-not-found-even-if-in-path
@@ -14,16 +14,19 @@ RUN apk add --no-cache libc6-compat
 ARG BUILD_VERSION
 ARG LOG_LEVEL=info
 ARG LOG_LABEL=bull-monitor
+ARG ALTERNATE_PORT=8081
 ARG PORT=3000
+ARG OAUTH2_PROXY_SKIP_AUTH_ROUTES='/metrics,/health,/docs'
 WORKDIR /app
-COPY --from=go /go/bin/oauth2_proxy ./
+COPY --from=go /go/bin/oauth2-proxy ./
 COPY --from=build /app ./
 COPY docker-entrypoint.sh .
-COPY oauth2_proxy.cfg .
 ENV NODE_ENV="production" \
+    ALTERNATE_PORT=$ALTERNATE_PORT \
     PORT=$PORT \
     LOG_LEVEL=$LOG_LEVEL \
     LOG_LABEL=$LOG_LABEL \
+    OAUTH2_PROXY_SKIP_AUTH_ROUTES=$OAUTH2_PROXY_SKIP_AUTH_ROUTES \
     VERSION=$BUILD_VERSION
 EXPOSE 3000
 ENTRYPOINT ["sh", "docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,37 @@
-# https://oauth2-proxy.github.io/oauth2-proxy/docs/configuration/overview#environment-variables
-export OAUTH2_PROXY_COOKIE_SECRET=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | base64 | tr -d -- '\n' | tr -- '+/' '-_'; echo)
-export OAUTH2_PROXY_COOKIE_NAME="oauth2_proxy"
+#!/bin/sh
+
+CONFIG_FILE=oauth2_proxy.cfg
+STARTUP_COMMAND="node daemon.js"
+OAUTH2_PROXY="./oauth2-proxy"
+LOG_TRANSFORMER="node transform_logs.js"
+
+# https://serverfault.com/questions/599103/make-a-docker-application-write-to-stdout
+DOCKER_STDOUT="/proc/1/fd/1"
+
+# enable prod logging if NODE_ENV is production
+if [[ "$NODE_ENV" = "production" ]]; then
+  LOG_TRANSFORMER="$LOG_TRANSFORMER --production"
+fi
+
+if [[ -z "$ALTERNATE_PORT" ]]; then
+    echo "Environment variable not defined: ALTERNATE_PORT"
+    exit 1
+fi
 
 if [[ "$USE_OAUTH2_PROXY" == "1" ]]; then
     echo "OAuth2 Proxy Enabled"
-    # need to run service on a different port to make room for proxy
-    export PORT=8081
+    # https://oauth2-proxy.github.io/oauth2-proxy/docs/configuration/overview#environment-variables
+    export OAUTH2_PROXY_COOKIE_SECRET=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | base64 | tr -d -- '\n' | tr -- '+/' '-_'; echo)
+    export OAUTH2_PROXY_COOKIE_NAME="oauth2_proxy"
+    export OAUTH2_PROXY_HTTP_ADDRESS="http://0.0.0.0:${PORT}"
+    export OAUTH2_PROXY_UPSTREAMS="http://0.0.0.0:${ALTERNATE_PORT}"
+
     # https://serverfault.com/questions/599103/make-a-docker-application-write-to-stdout
-    ./oauth2_proxy --config oauth2_proxy.cfg --redirect-url $OAUTH2_PROXY_REDIRECT_URL  > /proc/1/fd/1 &
+    $OAUTH2_PROXY 2>&1 | $LOG_TRANSFORMER  > $DOCKER_STDOUT &
+    
+    # need to run service on a different port to make room for proxy
+    PORT=$ALTERNATE_PORT $STARTUP_COMMAND
 else
     echo "OAuth2 Proxy Disabled"
+    $STARTUP_COMMAND
 fi
-
-node daemon.js
